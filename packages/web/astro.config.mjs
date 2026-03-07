ef06cd4f22e1206f57d17e2a9f3c881639dd2bd1
webshoten <weboshimashoten@gmail.com>
2026-03-07 14:25:03 +0900

    sstv3->v4

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import aws from 'astro-sst';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: aws(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
